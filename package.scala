bc74ac66d3bae3d1c2649f34befd33f8641c3584
lfbhlx <1203550038@qq.com>
2017-12-22 10:04:54 +0800

    Add files via upload

---
+++ package.scala
@@ -0,0 +1,11 @@
+import com.thoughtworks.binding.Binding
+import com.thoughtworks.binding.Binding.BindingSeq
+import org.scalajs.dom.Node
+
+import scala.xml.NodeBuffer
+
+package object mypackage {
+  implicit def makeIntellijHappy[T<:Any](x:T):Binding[T] = ???
+  implicit def makeIntellijHappy[T<:org.scalajs.dom.raw.Node](x: scala.xml.Node): Binding[T] = ???
+  implicit def makeIntellijHappy(x:NodeBuffer):Binding[BindingSeq[Node]] = ???
+}
