291da1cc38224f04e329c1930b34eae7b06012af
lfbhlx <1203550038@qq.com>
2017-12-23 10:29:01 +0800

    Update package.scala

--- package.scala
+++ package.scala
@@ -8,4 +8,5 @@ package object mypackage {
   implicit def makeIntellijHappy[T<:Any](x:T):Binding[T] = ???
   implicit def makeIntellijHappy[T<:org.scalajs.dom.raw.Node](x: scala.xml.Node): Binding[T] = ???
   implicit def makeIntellijHappy(x:NodeBuffer):Binding[BindingSeq[Node]] = ???
+  implicit def makeIntellijHappy[I<:Elem,O<:HTMLElement](e:I):O = ???
 }
